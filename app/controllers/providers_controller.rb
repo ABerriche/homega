f3602ad1f99282abf8b32a92316ac4a4695b0d55
Jean-Philippe BOUL <jean.philippe.boul@gmail.com>
2016-11-29 11:13:30 +0100

    controller done

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,2 +1,26 @@
 class ProvidersController < ApplicationController
+
+   before_action :set_provider, only: [:show]
+
+  def show
+    @provider = Provider.find(params[:id])
+  end
+
+
+  def index
+    @providers = Provider.all
+  end
+
+
+  private
+
+  def set_provider
+    @user = Provider.find(params[:id])
+  end
+
+  def provider_params
+    params.require(:user).permit(:first_name, :last_name, :address, :category, :company)
+  end
+
 end
+
