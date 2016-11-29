cda19673fd90a502821cb045035e03bf8b7a8a7c
Denis Khuon <denis.khuon@gmail.com>
2016-11-29 11:58:10 +0100

    formulaire damage dynamique si vol

--- app/models/damage.rb
+++ app/models/damage.rb
@@ -3,7 +3,7 @@ class Damage < ApplicationRecord
   has_many :bookings, dependent: :destroy
 
   validates :place_id, presence: true
-  validates :category, presence: true
+  validates :category, presence: true, inclusion: {in: ["Dégâts des eaux", "Vol", "Incendie", "Catastrophe naturelle"] }
   validates :responsability, presence: true
   validates :value_thief, presence: true
   validates :cat_water_damage, presence: true
