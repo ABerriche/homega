2fdf5c5071215b46e28bd46cd84365ed64f9d956
Alexandre Berriche <alexandre.berriche@gmail.com>
2016-11-29 16:35:26 +0100

    fooooter

--- db/schema.rb
+++ db/schema.rb
@@ -56,9 +56,9 @@ ActiveRecord::Schema.define(version: 20161128173234) do
     t.string   "trustee_reference"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
-    t.string   "name"
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "name"
     t.index ["user_id"], name: "index_places_on_user_id", using: :btree
   end
 
