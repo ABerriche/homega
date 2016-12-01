5e5dd11df4f383290fb1e721c7044744b833052e
Alexandre Berriche <alexandre.berriche@gmail.com>
2016-12-01 18:25:06 +0100

    regex rib

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -4,5 +4,7 @@ class Quote < ApplicationRecord
   validates :price, presence: true
   validates :covered_amount, presence: true
   validates :place_id, presence: true
+  validates :rib, format: { with: /FR\d{2}[ ]\d{4}[ ]\d{4}[ ]\d{4}[ ]\d{4}[ ]\d{4}|CZ\d{22}/}
+
 
 end
