cd7cc62d44a35aa1e0d8dcfeac7287cb532e591a
Denis Khuon <denis.khuon@gmail.com>
2016-12-05 18:02:33 +0100

    sinistre KO avec javascript

--- app/controllers/damages_controller.rb
+++ app/controllers/damages_controller.rb
@@ -15,6 +15,7 @@ class DamagesController < ApplicationController
     place_id = current_user.places.where(name: params[:damage][:place_id]).as_json[0]["id"]
     @damage = Damage.new(damage_params)
     @damage.place_id = place_id
+    raise
     if @damage.save
       redirect_to damage_path(@damage)
     else
